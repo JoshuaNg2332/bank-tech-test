0f0aebe65c395cb584f851dbe3958ae6b9e93c12
Josh <joshua.ng2332@gmail.com>
2020-08-14 12:02:34 +0100

    Deleted old commentented out code.

--- lib/account.rb
+++ lib/account.rb
@@ -11,30 +11,21 @@ class Account
   def initialize
     @current_balance = 0
     @transactions = []
-    # @date = DateTime.now.strftime '%d/%m/%Y'
   end
 
   def deposit(amount)
     @current_balance += amount
     create_transaction(amount)
-
-    #@transactions << "#{@date} ||  #{'%.2f' % amount}  ||    ||  #{'%.2f' % @current_balance}"
   end
 
   def withdraw(amount)
     raise 'Your withdrawal exceeds your current balance.' if amount > @current_balance
     @current_balance -= amount
     create_transaction(amount, 'credit')
-
-    #@transactions << "#{@date} ||    ||  #{'%.2f' % amount}  ||  #{'%.2f' % @current_balance}"
   end
 
   def statement(statement_class = Statement)
     statement_class.print(@transactions.reverse)
-    # statement = Statement.new
-    # statement.print
-    # transaction = @transactions.reverse.join("\n")
-    # puts statement.headers + transaction
   end
 
   def create_transaction(amount, type=nil, transaction_class = Transaction)
